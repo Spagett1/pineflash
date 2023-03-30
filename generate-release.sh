b902bb0f141d832cddad8ab5056ead8da244e72e
Spagett1 <laar@tutanota.com>
2023-03-30 14:49:57 +1030

    Added helper script to generate releases

---
+++ generate-release.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ver=$(grep "^version" Cargo.toml | cut -d\" -f2)
+mkdir ./release-build/
+mkdir ./release-out/
+cp ./PKGBUILD ./release-build/
+cd ./release-build/
+makepkg
+cp pineflash-git*.tar.zst ../release-out/pineflash-$ver.tar.zst
+cd pkg/pineflash-git/
+rm .*
+zip -r pineflash_linux_x86_$ver.zip ./
+mv ./pineflash_linux_x86_$ver.zip ../../../release-out/
