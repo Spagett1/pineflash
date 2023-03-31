136103f78d033512658f899fd06f321af71bfe2d
Spagett1 <laar@tutanota.com>
2023-03-31 23:00:35 +1030

    updating generic install script

--- generic_linux_install.sh
+++ generic_linux_install.sh
@@ -6,6 +6,13 @@ else
 fi
 git submodule update --init --recursive
 cargo build --release
+cd blisp 
+mkdir build
+cd build 
+cmake -DBLISP_BUILD_CLI=ON ..
+cmake --build .
+cd ../..
+$root cp ./blist/build/tools/blisp/blisp /usr/bin/blisp
 $root cp ./Pineflash.desktop /usr/share/applications/Pineflash.desktop
 $root cp ./assets/pine64logo.png /usr/share/pixmaps/pine64logo.png
 $root cp ./target/release/pineflash /usr/bin/pineflash
