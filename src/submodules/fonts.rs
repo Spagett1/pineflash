4a1ca27051cf81b70a4742fada1b2fca0ee466f8
Spagett1 <laar@tutanota.com>
2023-04-22 16:37:57 +0930

    Adding Asian font support

--- src/submodules/fonts.rs
+++ src/submodules/fonts.rs
@@ -5,7 +5,17 @@ use crate::Flasher;
 impl Flasher {
     pub fn configure_fonts(cc: egui::Context) {
         let mut fonts = FontDefinitions::default();
-        // Imports the MesloLGS font from its ttf file in order to enable support for other characters
+        fonts.font_data.insert(
+            "Chinese".to_owned(),
+            FontData::from_static(include_bytes!("../../assets/XiaolaiSC-Regular.ttf")),
+        );
+        fonts
+            .families
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .push("Chinese".to_owned());
+
+        // Use meslolgs for icons
         fonts.font_data.insert(
             "MesloLGS".to_owned(),
             FontData::from_static(include_bytes!("../../assets/SFMono_Nerd_Font_Complete.otf")),
